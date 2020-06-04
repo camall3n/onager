310750418c74851c15ab4105a3f7febcb8faef4a
Cam Allen <csal@brown.edu>
2020-06-04 14:41:04 -0400

    Add job name and fix escaping of shell vars

--- backends/gridengine.py
+++ backends/gridengine.py
@@ -25,6 +25,7 @@ source ./venv/bin/activate
         # and passes a different taskid to each one. If ntasks is zero, only a
         # single job is submitted with no subtasks.
         base_cmd = 'qsub '
+        base_cmd += '-N {} '.format(args.jobname) # set name of job
         base_cmd += '-cwd ' # run script in current working directory
 
         # Duration and number of CPU/GPU resources
@@ -63,8 +64,8 @@ source ./venv/bin/activate
         # Logging
         log_dir = self.get_log_dir()
         # Format is jobname_jobid_taskid.*
-        base_cmd += '-o {} '.format(os.path.join(log_dir, '${JOB_NAME@Q}_${JOB_ID@Q}_${TASK_ID@Q}.o')) # save stdout to file
-        base_cmd += '-e {} '.format(os.path.join(log_dir, '${JOB_NAME@Q}_${JOB_ID@Q}_${TASK_ID@Q}.e')) # save stderr to file
+        base_cmd += '-o {} '.format(os.path.join(log_dir, r'\$JOB_NAME_\$JOB_ID_\$TASK_ID.o')) # save stdout to file
+        base_cmd += '-e {} '.format(os.path.join(log_dir, r'\$JOB_NAME_\$JOB_ID_\$TASK_ID.e')) # save stderr to file
 
         # The -terse flag causes qsub to print the jobid to stdout. We read the
         # jobid with subprocess.check_output(), and use it to delay the email job
