17f5d1c37a78598e5e6eac5e83080f8b80943862
neevparikh <neev_parikh@brown.edu>
2020-06-05 19:47:19 -0400

    Moved to constants.py, added args to prelaunch

---
+++ thoth/constants.py
@@ -0,0 +1,6 @@
+import os
+
+# Basic constants
+default_scripts_folder = os.path.join('.thoth', 'scripts')
+default_logs_folder = os.path.join('.thoth', 'logs')
+defaultjobfile = os.path.join(default_scripts_folder, '{jobname}', 'jobs.json')
