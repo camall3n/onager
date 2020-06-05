3243dfe8ef5d047cb086fe8d9cf25f939c5ef2e8
neevparikh <neev_parikh@brown.edu>
2020-06-05 19:54:17 -0400

    Formatting changes on meta launcher

--- thoth/meta_launcher.py
+++ thoth/meta_launcher.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 import json
 import os
 
+
 def meta_launch(args):
     base_cmd = args.command
 
@@ -16,23 +17,33 @@ def meta_launch(args):
 
     sep = ''
     for key, value_list in variables.items():
-        cmd_prefix_list = [prefix+' '+key+' {}' for prefix in cmd_prefix_list]
+        cmd_prefix_list = [prefix + ' ' + key + ' {}' for prefix in cmd_prefix_list]
         cmd_prefix_list = [prefix.format(v) for v in value_list for prefix in cmd_prefix_list]
         if args.tag_name is not None:
             if key in args.tag_args:
-                value_slot = '-{}' if len(value_list) > 1 or value_list[0]!='' else '{}'
-                keyname = key.replace('-','').replace('_','')
-                cmd_suffix_list = [suffix+sep+keyname+value_slot for suffix in cmd_suffix_list]
-                cmd_suffix_list = [suffix.format(v) for v in value_list for suffix in cmd_suffix_list]
+                value_slot = '-{}' if len(value_list) > 1 or value_list[0] != '' else '{}'
+                keyname = key.replace('-', '').replace('_', '')
+                cmd_suffix_list = [
+                    suffix + sep + keyname + value_slot for suffix in cmd_suffix_list
+                ]
+                cmd_suffix_list = [
+                    suffix.format(v) for v in value_list for suffix in cmd_suffix_list
+                ]
             else:
                 cmd_suffix_list = [suffix for v in value_list for suffix in cmd_suffix_list]
             sep = '_'
 
     if args.tag_id is not None:
-        cmd_suffix_list = [args.tag_id + '-{}_'.format(i) + suffix for (i, suffix) in enumerate(cmd_suffix_list, args.tag_id_start)]
+        cmd_suffix_list = [
+            args.tag_id + '-{}_'.format(i) + suffix
+            for (i, suffix) in enumerate(cmd_suffix_list, args.tag_id_start)
+        ]
 
     if args.tag_name is not None:
-        cmd_prefix_list = [' '.join([prefix, '--'+args.tag_name, suffix]) for (prefix, suffix) in zip(cmd_prefix_list, cmd_suffix_list)]
+        cmd_prefix_list = [
+            ' '.join([prefix, '--' + args.tag_name, suffix])
+            for (prefix, suffix) in zip(cmd_prefix_list, cmd_suffix_list)
+        ]
 
     jobfile_path = args.jobfile.format(jobname=args.tag_name)
     os.makedirs(os.path.dirname(jobfile_path), exist_ok=True)
