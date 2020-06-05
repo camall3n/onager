81907531ca92220cbe54adb2f650259df9c450f3
Cam Allen <csal@brown.edu>
2020-06-05 15:53:34 -0400

    Convert meta_launcher to use '+' args instead of '-'

--- meta_launcher.py
+++ meta_launcher.py
@@ -12,45 +12,35 @@ def usage_msg():
         [--meta-tag-name tagname]
         [--meta-tag-args [argA argC]]'''
 
-parser = argparse.ArgumentParser(description='Generate commands based on lists of arguments', usage=usage_msg())
-parser.add_argument('--meta-command', type=str, required=True)
-parser.add_argument('--meta-tag-name', type=str)
-parser.add_argument('--meta-tag-args', type=str, nargs='*')
-parser.add_argument('--meta-tag-id', type=str)
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description='Generate commands based on lists of arguments',
+                                 prefix_chars='+')
+parser.add_argument('+arg', type=str, action='append', nargs='+',
+                    metavar=('argname', '[value, ...]'),
+                    help='Add an argument with zero or more mutually exclusive values')
+parser.add_argument('+command', type=str, required=True)
+parser.add_argument('+tag-name', type=str)
+parser.add_argument('+tag-args', type=str, nargs='*')
+parser.add_argument('+tag-id', type=str)
 args, child_args = parser.parse_known_args()
 
-base_cmd = args.meta_command
+base_cmd = args.command
 
-variables = OrderedDict({None: []})
-current_var = None
-for arg in child_args:
-    if '--' == arg[:2]:
-        # variable
-        if current_var is not None and not variables[current_var]:
-            variables[current_var] = ['']
-        current_var = arg[2:]
-        variables[current_var] = []
-    else:
-        variables[current_var].append(arg)
-if current_var is not None and not variables[current_var]:
-    variables[current_var] = ['']
-if variables[None]:
-    base_cmd = base_cmd + ' ' + ' '.join(variables[None])
-del variables[None]
+variables = OrderedDict({arglist[0]: arglist[1:] for arglist in args.arg})
 
 cmd_prefix_list = [base_cmd]
 
-if args.meta_tag_name is not None:
+if args.tag_name is not None:
     cmd_suffix_list = ['']
-    if args.meta_tag_args is None:
-        args.meta_tag_args = list(variables.keys())
+    if args.tag_args is None:
+        args.tag_args = list(variables.keys())
 
 sep = ''
 for key, value_list in variables.items():
-    cmd_prefix_list = [prefix+' --'+key+' {}' for prefix in cmd_prefix_list]
+    cmd_prefix_list = [prefix+' '+key+' {}' for prefix in cmd_prefix_list]
     cmd_prefix_list = [prefix.format(v) for v in value_list for prefix in cmd_prefix_list]
-    if args.meta_tag_name is not None:
-        if key in args.meta_tag_args:
+    if args.tag_name is not None:
+        if key in args.tag_args:
             value_slot = '-{}' if len(value_list) > 1 or value_list[0]!='' else '{}'
             keyname = key.replace('-','').replace('_','')
             cmd_suffix_list = [suffix+sep+keyname+value_slot for suffix in cmd_suffix_list]
@@ -59,11 +49,11 @@ for key, value_list in variables.items():
             cmd_suffix_list = [suffix for v in value_list for suffix in cmd_suffix_list]
         sep = '_'
 
-if args.meta_tag_id is not None:
-    cmd_suffix_list = [args.meta_tag_id + '-{}_'.format(i) + suffix for (i, suffix) in enumerate(cmd_suffix_list)]
+if args.tag_id is not None:
+    cmd_suffix_list = [args.tag_id + '-{}_'.format(i) + suffix for (i, suffix) in enumerate(cmd_suffix_list)]
 
-if args.meta_tag_name is not None:
-    cmd_prefix_list = [' '.join([prefix, '--'+args.meta_tag_name, suffix]) for (prefix, suffix) in zip(cmd_prefix_list, cmd_suffix_list)]
+if args.tag_name is not None:
+    cmd_prefix_list = [' '.join([prefix, '--'+args.tag_name, suffix]) for (prefix, suffix) in zip(cmd_prefix_list, cmd_suffix_list)]
 
 for cmd in cmd_prefix_list:
     print(cmd)
