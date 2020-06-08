a7da5446c67839b523a9520b4458c437b91582a2
neevparikh <neev_parikh@brown.edu>
2020-06-08 14:25:57 -0400

    Renamed args for prelaunch

--- thoth/meta_launcher.py
+++ thoth/meta_launcher.py
@@ -11,7 +11,7 @@ def meta_launch(args):
 
     cmd_prefix_list = [base_cmd]
 
-    if args.tag_name is not None:
+    if args.tag is not None:
         cmd_suffix_list = ['']
         if args.tag_args is None:
             args.tag_args = list(variables.keys())
@@ -20,7 +20,7 @@ def meta_launch(args):
     for key, value_list in variables.items():
         cmd_prefix_list = [prefix + ' ' + key + ' {}' for prefix in cmd_prefix_list]
         cmd_prefix_list = [prefix.format(v) for v in value_list for prefix in cmd_prefix_list]
-        if args.tag_name is not None:
+        if args.tag is not None:
             if key in args.tag_args:
                 value_slot = '-{}' if len(value_list) > 1 or value_list[0] != '' else '{}'
                 keyname = key.replace('-', '').replace('_', '')
@@ -34,25 +34,25 @@ def meta_launch(args):
                 cmd_suffix_list = [suffix for v in value_list for suffix in cmd_suffix_list]
             sep = '_'
 
-    if args.tag_id is not None:
-        cmd_suffix_list = [
-            args.tag_id + '-{}_'.format(i) + suffix
-            for (i, suffix) in enumerate(cmd_suffix_list, args.tag_id_start)
-        ]
+    cmd_suffix_list = [
+        args.jobname + '-{}_'.format(i) + suffix
+        for (i, suffix) in enumerate(cmd_suffix_list, args.jobname_start)
+    ]
 
-    if args.tag_name is not None:
+    if args.tag is not None:
         cmd_prefix_list = [
-            ' '.join([prefix, '--' + args.tag_name, suffix])
+            ' '.join([prefix, '--' + args.tag, suffix])
             for (prefix, suffix) in zip(cmd_prefix_list, cmd_suffix_list)
         ]
 
-    jobfile_path = args.jobfile.format(jobname=args.tag_name)
+    jobfile_path = args.jobfile.format(jobname=args.jobname)
     os.makedirs(os.path.dirname(jobfile_path), exist_ok=True)
     if args.append:
         jobs = load_jobfile(jobfile_path)
     else:
         jobs = dict()
-        for i, cmd in enumerate(cmd_prefix_list, args.tag_id_start):
+    with open(jobfile_path, "w+") as jobfile:
+        for i, cmd in enumerate(cmd_prefix_list, args.jobname_start):
             if args.verbose:
                 print(cmd)
             jobs[i] = cmd
