6559a6dda5d1f2b930e63fdb054a9624fc70dba6
Cam Allen <csal@brown.edu>
2020-06-11 13:46:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@ from setuptools import setup, find_packages
 setup(
     name='onager',
     version='0.1.0',
-    description='Launching, logging and plotting experiments',
+    description='Lightweight python library for launching experiments and tuning hyperparameters, either locally or on a cluster',
     long_description=open('README.md').read(),
     license=open('LICENSE').read(),
-    author='Cameron Allen <csal@cs.brown.edu> , Neev Parikh <neev_parikh@brown.edu>',
+    author='Cameron Allen, Neev Parikh',
+    author_email=('csal@brown.edu, neev_parikh@brown.edu'),
     packages=find_packages(include=['onager', 'onager.*']),
     scripts=['bin/onager'],
+    url='http://pypi.python.org/pypi/onager/',
     install_requires=[
         "tabulate",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
