2a197eb271fe3e3e9fc6e30c09530ae6c6d2ac82
Cam Allen <csal@brown.edu>
2020-06-11 01:46:50 -0400

    Add config subcommand

---
+++ thoth/config.py
@@ -0,0 +1,68 @@
+import configparser
+import os
+from warnings import warn
+
+from .constants import globalconfigfile, localconfigfile
+from . import backends
+
+defaults = {
+    'slurm': {
+        'header': '',
+        'footer': '',
+    },
+    'gridengine': {
+        'header': '',
+        'footer': '',
+    }
+}
+
+def ensure_initialized():
+    settings = configparser.ConfigParser()
+    settings.read_dict(defaults)
+    if not os.path.exists(localconfigfile):
+        with open(localconfigfile, 'w') as config_file:
+            settings.write(config_file)
+
+def print_config(config):
+    for section in config.sections():
+        print('[{}]'.format(section))
+        for key, value in dict(config[section]).items():
+            quote = '"""'  if '\n' in value else ''
+            print('{} = {}{}{}'.format(key, quote, value, quote))
+        print()
+
+def config(args):
+    if args.read and args.write:
+        raise RuntimeError('Cannot read and write at the same time.')
+    if args.read and args.global_ and args.local:
+        args.global_ = False
+        args.local = False
+    if args.write and args.global_ and args.local:
+        raise RuntimeError('Cannot write to global and local config files simultaneously.')
+
+    settings = configparser.ConfigParser()
+    ensure_initialized()
+    settings.read_dict(defaults)
+    if args.global_ or not args.local:
+        settings.read(globalconfigfile)
+    if args.local or not args.global_:
+        settings.read(localconfigfile)
+    
+    if args.read:
+        print_config(settings)
+    elif args.write:
+        for (backend, key, value) in args.write:
+            if backend not in backends.__all__:
+                warn('Unable to set {}.{}={} (invalid backend: {}).'.format(backend, key, value, backend))
+                continue
+            # if key not in settings[backend].keys():
+            #     warn('Unable to set {}.{}={} (invalid key: {}).'.format(backend, key, value, key))
+            #     continue
+            settings.set(backend, key, value)
+        
+        if args.global_:
+            with open(globalconfigfile, 'w') as config_file:
+                settings.write(config_file)
+        else:
+            with open(localconfigfile, 'w') as config_file:
+                settings.write(config_file)
--- thoth/constants.py
+++ thoth/constants.py
@@ -5,6 +5,8 @@ default_scripts_folder = os.path.join('.thoth', 'scripts')
 default_logs_folder = os.path.join('.thoth', 'logs')
 default_index = os.path.join('.thoth', 'job_index.csv') # id,jobname,jobfile_path
 defaultjobfile = os.path.join(default_scripts_folder, '{jobname}', 'jobs.json')
+globalconfigfile = os.path.join(os.path.expanduser('~'), '.thothconfig')
+localconfigfile = os.path.join('.thoth', 'config')
 
 # Separators
 sep = '_'
