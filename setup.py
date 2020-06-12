add4636bcc94ca75a2a42f3390af75dbfa730e3d
Cam Allen <csal@brown.edu>
2020-06-11 21:53:05 -0400

    Tell PyPI description to use markdown syntax

--- setup.py
+++ setup.py
@@ -4,18 +4,14 @@ setup(
     version='0.1.0',
     description='Lightweight python library for launching experiments and tuning hyperparameters, either locally or on a cluster',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     license=open('LICENSE').read(),
     author='Cameron Allen, Neev Parikh',
-    author_email=('csal@brown.edu, neev_parikh@brown.edu'),
+    author_email=('csal@brown.edu,neev_parikh@brown.edu'),
     packages=find_packages(include=['onager', 'onager.*']),
     scripts=['bin/onager'],
     url='https://github.com/camall3n/onager/',
     install_requires=[
         "tabulate",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
 )
