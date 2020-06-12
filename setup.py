9c68d8a3bb5bc346c27b2790ae84d557920ec839
Cam Allen <csal@brown.edu>
2020-06-11 21:59:20 -0400

    Bump version number to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='onager',
-    version='0.1.0',
+    version='0.1.1',
     description='Lightweight python library for launching experiments and tuning hyperparameters, either locally or on a cluster',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
