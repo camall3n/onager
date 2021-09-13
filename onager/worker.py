ecc8cdd9310facd2c8f66f16cc4c009023357c33
Cam Allen <csal@brown.edu>
2021-09-13 02:12:11 -0400

    Print elapsed time when worker finishes

--- onager/worker.py
+++ onager/worker.py
@@ -1,4 +1,5 @@
 from contextlib import ExitStack
+from datetime import datetime
 import subprocess
 import sys
 
@@ -8,6 +9,7 @@ def run_command_by_id(commands, task_id, stdout=None, stderr=None, quiet=False):
     cmd = commands[task_id]
     if not quiet:
         print('Launching worker:', cmd)
+        start_time = datetime.now()
     with ExitStack() as stack:
         stdout = stack.enter_context(open(stdout, 'wb')) if stdout is not None else None
         stderr = stack.enter_context(open(stderr, 'wb')) if stderr is not None else None
@@ -17,11 +19,13 @@ def run_command_by_id(commands, task_id, stdout=None, stderr=None, quiet=False):
             raise
         else:
             if not quiet:
+                elapsed = datetime.now() - start_time
                 print('Worker finished:', cmd)
+                print('Elapsed time:  {}'.format(elapsed))
 
 if __name__ == '__main__':
     assert len(sys.argv) == 3, 'Usage: python -m worker path/to/commands.json task_id'
-    
+
     commands_file = sys.argv[1]
     task_id = int(sys.argv[2])
 
