b4791c84a61d760b471a1996d2ee857b083e9f5f
Cam Allen <csal@brown.edu>
2022-02-04 16:24:35 -0500

    [WIP] Add draft gridengine functionality

--- onager/backends/gridengine.py
+++ onager/backends/gridengine.py
@@ -2,7 +2,8 @@ from datetime import timedelta
 import os
 
 from ._backend import Backend
-from ..utils import condense_ids
+from ..subjobsfilemanager import SubjobsFileManager
+from ..utils import compute_subjobs_filename, split_tasklist_into_subjob_groups, condense_ids
 
 class GridEngineBackend(Backend):
     def __init__(self):
@@ -92,11 +93,22 @@ class GridEngineBackend(Backend):
             # set maximum number of running tasks per block
             base_cmd += "-tc {} ".format(args.max_tasks)
 
+        if args.tasks_per_node == 1:
+            tasklist = args.tasklist
+            wrapper_filename = 'wrapper.sh'
+        elif args.tasks_per_node > 1:
+            list_of_tasklist_strings = split_tasklist_into_subjob_groups(args.tasklist, args.tasks_per_node)
+            subjobs_filename = compute_subjobs_filename(args.jobfile)
+            sfm = SubjobsFileManager(subjobs_filename)
+            subjob_groupids = sfm.add_subjobs(list_of_tasklist_strings)
+            tasklist = condense_ids(subjob_groupids)
+            wrapper_filename = 'multiwrapper.sh'
+
         wrapper_script = self.wrap_tasks(args.jobfile, args)
-        wrapper_file = self.save_wrapper_script(wrapper_script, args.jobname)
+        wrapper_file = self.save_wrapper_script(wrapper_script, args.jobname, filename=wrapper_filename)
 
         # Split tasklist into blocks that GridEngine can understand
-        task_blocks = args.tasklist.split(',')
+        task_blocks = tasklist.split(',')
         return [
             base_cmd + "-t {} {}".format(task_block, wrapper_file) for task_block in task_blocks
         ]
